7f69ca0611e05fb4d531898fbc9d8a965b629c67
Ethan Yang <ethan.y@outlook.com>
2020-01-26 21:13:51 +1100

    SockMerchant

---
+++ HackRank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HackRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int n = 9;
+            int[] ar = { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            int result = SockMerchant.sockMerchant(n, ar);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HackRank/SockMerchant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackRank
+{
+    class SockMerchant
+    {
+        public static int sockMerchant(int n, int[] ar)
+        {
+            int numberOfPairs = 0;
+            HashSet<int> sockSet = new HashSet<int>();
+            foreach (int sock in ar)
+            {
+                if (sockSet.Contains(sock))
+                {
+                    numberOfPairs++;
+                    sockSet.Remove(sock);
+                } else
+                {
+                    sockSet.Add(sock);
+                }
+            }
+            return numberOfPairs;
+        }   
+    }
+}
