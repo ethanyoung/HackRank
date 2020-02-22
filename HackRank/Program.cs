f0ba0ae4826a0ae0fad16254b0559844f7150248
Ethan Yang <ethan.y@outlook.com>
2020-02-22 17:32:23 +1100

    Add RepeatedString

--- HackRank/Program.cs
+++ HackRank/Program.cs
@@ -6,11 +6,18 @@ namespace HackRank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sock Merchant");
             int n = 9;
             int[] ar = { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
-            int result = SockMerchant.sockMerchant(n, ar);
-            Console.WriteLine(result);
+            int result1 = SockMerchant.sockMerchant(n, ar);
+            Console.WriteLine(result1);
+
+            Console.WriteLine("Repeated String");
+            string str = "aba";
+            long numberOfChars = 10;
+            long result2 = RepeatedString.repeatedString(str, numberOfChars);
+            Console.WriteLine(result2);
+
         }
     }
 }
---
+++ HackRank/RepeatedString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackRank
+{
+    class RepeatedString
+    {
+        public static long repeatedString(string s, long n)
+        {
+            var numberOfOccurence = n / s.Length;
+            var extraChars = n % s.Length;
+
+            int numberOfOccurenceInString = 0;
+            int numberOfOccurenceStoppedAtExtra = 0;
+            int index = 0;
+            foreach (char c in s)
+            {
+                if (c == 'a')
+                {
+                    numberOfOccurenceInString++;
+                }
+                if (index + 1 == extraChars)
+                {
+                    numberOfOccurenceStoppedAtExtra = numberOfOccurenceInString;
+                }
+                index++;
+            }
+
+            long totalOccurence = numberOfOccurence * numberOfOccurenceInString + numberOfOccurenceStoppedAtExtra;
+            return totalOccurence;
+        }
+    }
+}
